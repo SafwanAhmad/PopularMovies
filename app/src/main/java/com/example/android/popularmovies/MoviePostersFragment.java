582389d6ef2c5ae73218774330f4d9815e51d845
Safwan Ahmad <safwan.pico@gmail.com>
2016-10-23 18:20:13 +0530

    Renamed.

---
+++ app/src/main/java/com/example/android/popularmovies/MoviePostersFragment.java
@@ -0,0 +1,124 @@
+package com.example.android.popularmovies;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MoviePostersFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MoviePostersFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MoviePostersFragment extends Fragment {
+
+    //The reference to the adapter connected to the grid view
+    ImageAdapter mImageAdapter = null;
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MoviePostersFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MoviePostersFragment newInstance(String param1, String param2) {
+        MoviePostersFragment fragment = new MoviePostersFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MoviePostersFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View fragmentMoviePosters = inflater.inflate(R.layout.fragment_movie_posters, container, false);
+
+        //Accessing the grid view inside the this fragment
+        GridView gridView = (GridView)fragmentMoviePosters.findViewById(R.id.main_grid_view);
+
+        //Attach a adapter with this grid view
+        mImageAdapter = new ImageAdapter(getActivity());
+        gridView.setAdapter(mImageAdapter);
+
+        return fragmentMoviePosters;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
