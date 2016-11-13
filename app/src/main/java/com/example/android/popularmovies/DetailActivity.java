61634ede51b16bc060fd941d274ef54e963a1d88
Safwan Ahmad <safwan.pico@gmail.com>
2016-11-13 17:12:32 +0530

    Detail Activity for devices not having two pane UI.

---
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -0,0 +1,67 @@
+package com.example.android.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class DetailActivity extends ActionBarActivity implements MovieDetailFragment.OnDetailFragmentInteractionListener {
+
+    public static final String MOVIE_ID_KEY = "MOVIE_ID";
+
+    //Tag for movie details fragment
+    private static final String MOVIE_DETAIL_FRAG_TAG = "MDFTAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        //This activity is launched via intent when device doesn't
+        //support two pane UI
+        if(savedInstanceState ==null) {
+            Bundle id = new Bundle();
+            id.putString(MovieDetailFragment.MOVIE_ID_KEY, getIntent().getStringExtra(MOVIE_ID_KEY));
+
+            MovieDetailFragment detailFragment = new MovieDetailFragment();
+            detailFragment.setArguments(id);
+
+            getSupportFragmentManager().beginTransaction().
+                    add(R.id.container_fragment_movie_details, detailFragment, MOVIE_DETAIL_FRAG_TAG).commit();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDetailFragmentInteraction(String[] movieDetails) {
+        final FragmentTransaction ft =  getSupportFragmentManager().beginTransaction();
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(MOVIE_DETAIL_FRAG_TAG);
+
+        ft.detach(fragment).attach(fragment).commit();
+    }
+}
