390c291233fd65c64dd571f5a5604ff2cefd81aa
Safwan Ahmad <safwan.pico@gmail.com>
2016-11-14 23:18:49 +0530

    Adding preference using PreferenceFragment now.

---
+++ app/src/main/java/com/example/android/popularmovies/SettingFragment.java
@@ -0,0 +1,68 @@
+package com.example.android.popularmovies;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
+
+
+    public SettingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        //Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.general_prefs);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTheme(R.style.SettingTheme);
+
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_sorting_order_key)));
+    }
+
+    /**
+     * Attaches a listener so the summary is always updated with the preference value.
+     * Also fires the listener once, to initialize the summary (so it shows up before the value
+     * is changed.)
+     */
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        //Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(this);
+
+        //Also trigger the listener immediately with the preference's
+        //current value.
+        onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String stringValue = newValue.toString();
+
+        //For list preferences, look up the correct display value in the preference's
+        //'entries' list (since they have separate labels/values).
+        ListPreference listPreference = (ListPreference) preference;
+        int prefIndex = listPreference.findIndexOfValue(stringValue);
+
+        if (prefIndex >= 0) {
+            //Set the summary field
+            listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+        }
+        return true;
+    }
+
+}
