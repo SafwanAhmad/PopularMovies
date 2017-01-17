b9de1c01fa708f9841f73802d536a1332cf0cedc
Safwan Ahmad <safwan.pico@gmail.com>
2017-01-18 00:54:38 +0530

    Added Loader callbacks to loading data from database with the help of cursorLoader.

--- app/src/main/java/com/example/android/popularmovies/MoviePostersFragment.java
+++ app/src/main/java/com/example/android/popularmovies/MoviePostersFragment.java
@@ -1,14 +1,23 @@
 package com.example.android.popularmovies;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import com.example.android.popularmovies.data.MovieContract;
 
 
 /**
@@ -17,14 +26,41 @@ import android.widget.Toast;
  * {@link OnGridItemClickedListener} interface
  * to handle interaction events.
  */
-public class MoviePostersFragment extends Fragment {
+public class MoviePostersFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     //The reference to the adapter connected to the grid view
     ImageAdapter mImageAdapter = null;
 
-
+    //Parent activity to which this fragment is attached
     private OnGridItemClickedListener mListener;
 
+    //Projection string to be used for cursor loader
+    private static final String[] PROJECTION_POPULAR = {
+            MovieContract.Popular._ID,
+            MovieContract.Popular.COLUMN_MOVIE_POSTER_PATH
+    };
+
+    private static final String[] PROJECTION_TOP_RATED = {
+            MovieContract.TopRated._ID,
+            MovieContract.TopRated.COLUMN_MOVIE_POSTER_PATH
+    };
+
+    private static final String[] PROJECTION_FAVORITE = {
+            MovieContract.Favorite._ID,
+            MovieContract.Favorite.COLUMN_MOVIE_POSTER_PATH
+    };
+
+    //Based on above projections the index for columns. So if the projection changes
+    //this too have to be changed. The cursor returned will have columns in this order
+    //and follow these indices for columns
+    static final int COLUMN_ID = 0;
+    static final int COLUMN_MOVIE_POSTER_PATH = 1;
+
+
+    //Create three loaders each for available sorting orders
+    private final int loaderPopularId = 0;
+    private final int loaderTopRatedId = 1;
+    private final int loaderFavoriteId = 2;
 
     public MoviePostersFragment() {
         // Required empty public constructor
@@ -33,7 +69,23 @@ public class MoviePostersFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mImageAdapter = new ImageAdapter(getActivity());
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Get the type of loader based on the setting
+        String sortingOrder = getSortingOrder();
+
+        if (sortingOrder.equals(getString(R.string.pref_sorting_popular))) {
+            //Initialize loader
+            getLoaderManager().initLoader(loaderPopularId, savedInstanceState, this);
+        } else if (sortingOrder.equals(getString(R.string.pref_sorting_top_rated))) {
+            getLoaderManager().initLoader(loaderTopRatedId, savedInstanceState, this);
+        } else if (sortingOrder.equals(getString(R.string.pref_sorting_favorite))) {
+            getLoaderManager().initLoader(loaderFavoriteId, savedInstanceState, this);
+        }
     }
 
     @Override
@@ -42,8 +94,11 @@ public class MoviePostersFragment extends Fragment {
         // Inflate the layout for this fragment
         View fragmentMoviePosters = inflater.inflate(R.layout.fragment_movie_posters, container, false);
 
+        //Create the adapter without the cursor
+        mImageAdapter = new ImageAdapter(getActivity(), null, 0);
+
         //Accessing the grid view inside the this fragment
-        GridView gridView = (GridView)fragmentMoviePosters.findViewById(R.id.main_grid_view);
+        GridView gridView = (GridView) fragmentMoviePosters.findViewById(R.id.main_grid_view);
 
         //Attach a adapter with this grid view
         gridView.setAdapter(mImageAdapter);
@@ -53,18 +108,19 @@ public class MoviePostersFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //For debugging purpose
-                Toast.makeText(getActivity(),"Item["+position+"] clicked!", Toast.LENGTH_SHORT).show();
-                onPosterClicked(position);
+                Toast.makeText(getActivity(), "Item[" + position + "] clicked!", Toast.LENGTH_SHORT).show();
+                Cursor cursor = (Cursor) parent.getItemAtPosition(position);
+                String movieId = cursor.getString(COLUMN_ID);
+                onPosterClicked(movieId);
             }
         });
 
         return fragmentMoviePosters;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onPosterClicked(int position) {
+    public void onPosterClicked(String movieId) {
         if (mListener != null) {
-            mListener.onMoviePosterClicked(position);
+            mListener.onMoviePosterClicked(movieId);
         }
     }
 
@@ -85,6 +141,76 @@ public class MoviePostersFragment extends Fragment {
         mListener = null;
     }
 
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        CursorLoader cursorLoader = null;
+
+        switch (id)
+        {
+            case loaderPopularId: {
+                cursorLoader = new CursorLoader(getContext(),
+                        MovieContract.Popular.CONTENT_URI,
+                        PROJECTION_POPULAR,
+                        null,
+                        null,
+                        null);
+            }
+            break;
+
+            case loaderTopRatedId: {
+                cursorLoader = new CursorLoader(getContext(),
+                        MovieContract.TopRated.CONTENT_URI,
+                        PROJECTION_TOP_RATED,
+                        null,
+                        null,
+                        null);
+            }
+            break;
+
+            case loaderFavoriteId: {
+                cursorLoader = new CursorLoader(getContext(),
+                        MovieContract.Favorite.CONTENT_URI,
+                        PROJECTION_FAVORITE,
+                        null,
+                        null,
+                        null);
+            }
+            break;
+
+            default: {
+                throw new IllegalArgumentException("Loader id does not match");
+            }
+        }
+
+
+
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mImageAdapter.swapCursor((Cursor) data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mImageAdapter.swapCursor(null);
+    }
+
+    //Helper method to find the current sorting order
+    private String getSortingOrder() {
+        SharedPreferences preferences = (SharedPreferences) PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        String sortingOrder = preferences.getString(
+                getString(R.string.pref_sorting_order_key),
+                getString(R.string.pref_sorting_popular)
+        );
+
+        return sortingOrder;
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -96,8 +222,7 @@ public class MoviePostersFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnGridItemClickedListener {
-        // TODO: Update argument type and name
-        public void onMoviePosterClicked(int position);
+        public void onMoviePosterClicked(String movieId);
     }
 
 }
